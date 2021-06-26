2ed69781e457e151698ce834a9de655fc0c3b4f8
rsoapi1edu <86511668+rsoapi1edu@users.noreply.github.com>
2021-06-26 12:46:56 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Casmeric-Pyloom : USER : rsoapi1edu";
+echo "<br>";
+echo "Author : DOM : edumail.ml";
+echo "<br>";
+echo "Created : DATE : 26-06-2021";
+echo "<br>";
+echo date('d-m-Y').' : DAY : '.date('l'); 
